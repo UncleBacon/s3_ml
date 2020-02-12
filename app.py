749aa5cc13ed66250daf15c561079aaabdc995f5
UncleBacon <47467787+UncleBacon@users.noreply.github.com>
2020-02-11 19:09:03 -0500

    pushing local directory to repo

---
+++ app.py
@@ -0,0 +1,166 @@
+# Dependencies for Flask
+import json
+from flask import Flask, render_template, request, jsonify
+
+
+# Dependencies to run from AWS S3
+import boto3
+from boto.s3.key import Key
+from werkzeug.utils import secure_filename
+from skimage import io
+from skimage.transform import resize
+from config import ACCESS_KEY, SECRET_KEY,BUCKET_NAME
+
+# Dependenciees for all models
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Model
+import numpy as np
+
+
+# Dependencies for VGG16
+from tensorflow.keras.applications.vgg16 import (
+        VGG16, 
+        preprocess_input as preprocess_input_vgg16, 
+        decode_predictions as decode_predictions_vgg16
+)
+# Dependencies for VGG19
+from tensorflow.keras.applications.vgg19 import (
+        VGG19, 
+        preprocess_input as preprocess_input_vgg19, 
+        decode_predictions as decode_predictions_vgg19
+)
+# Dependencies for RESNET50
+from tensorflow.keras.applications.resnet50 import (
+        ResNet50,
+        preprocess_input as preprocess_input_resnet50, 
+        decode_predictions as decode_predictions_resnet50
+)
+
+# Initialize the app
+app = Flask(__name__)
+
+# Connect to S3 Bucket
+bucket_name = BUCKET_NAME 
+# app.secret_key = '213lkj' #Not sure if this is needed
+
+s3 = boto3.client(
+   "s3",
+   aws_access_key_id=ACCESS_KEY,
+   aws_secret_access_key=SECRET_KEY
+)
+bucket_resource = s3
+
+# APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.after_request
+def add_header(r):
+    # """
+    # Add headers to both force latest IE rendering engine or Chrome Frame,
+    # and also to cache the rendered page for 10 minutes.
+    # """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/models")
+def model():
+    return render_template("models.html")
+
+@app.route("/references")
+def references():
+    return render_template("references.html")
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    for file in request.files.getlist("file"):
+        # print(file)
+        if file:
+            filename = secure_filename(file.filename)
+            filename = 'test.jpg'
+            file.save(filename)
+            s3.upload_file(
+                Bucket = bucket_name,
+                Filename = filename,
+                Key = filename,
+                ExtraArgs={'ACL':'public-read'}
+            )
+
+    return render_template("predictions.html")
+
+@app.route("/predict")
+def predict_image():
+    ## For all Models
+    # Load Image
+    x = io.imread("https://rumlimages.s3.us-east-2.amazonaws.com/test.jpg")
+    x = resize(x, (224,224))
+    # Reshape the image according to the model input requierements
+    x = x.reshape((1, x.shape[0], x.shape[1], x.shape[2]))
+
+    # For Model VGG16
+    model = VGG16()
+    # Preprocess for the model VGG16
+    x_vgg16 = preprocess_input_vgg16(x)
+    # Prediction for the Model VGG16
+    prediction_vgg16 = model.predict(x_vgg16)
+    # Decoded Prediction for Model VGG16
+    decoded_prediction_vgg16 = decode_predictions_vgg16(prediction_vgg16, top=3)
+
+    # For Model VGG19
+    model = VGG19()
+    # Preprocess for the model VGG19
+    x_vgg19 = preprocess_input_vgg19(x)
+    # Prediction for the Model VGG19
+    prediction_vgg19 = model.predict(x_vgg19)
+    # Decoded Prediction for Model VGG19
+    decoded_prediction_vgg19 = decode_predictions_vgg19(prediction_vgg19, top=3)
+
+    # For Model RESNET50
+    model = ResNet50()
+    # Preprocess for the model RESNET50
+    x_restnet50 = preprocess_input_resnet50(x)
+    # Prediction for the Model RESNET50
+    prediction_resnet50 = model.predict(x_restnet50)
+    # Decoded Prediction for Model RESNET50
+    decoded_prediction_resnet50 = decode_predictions_resnet50(prediction_resnet50, top=3)
+
+    json_answer = []
+
+    model_vgg16_resp = []
+    for i in range(0,3):
+        json = {}
+        json["Model"] = 'VGG16'
+        json["no"] = i+1
+        json["prediction"] = decoded_prediction_vgg16[0][i][1],
+        json["probability"] = "{:.2%}".format(decoded_prediction_vgg16[0][i][2])
+        model_vgg16_resp.append(json)
+    json_answer.append(model_vgg16_resp)
+    model_vgg19_resp = []
+    for i in range(0,3):
+        json = {}
+        json["Model"] = 'VGG19'
+        json["no"] = i+1
+        json["prediction"] = decoded_prediction_vgg19[0][i][1],
+        json["probability"] = "{:.2%}".format(decoded_prediction_vgg19[0][i][2])
+        model_vgg19_resp.append(json)
+    json_answer.append(model_vgg19_resp)
+    model_resnet50 = []
+    for i in range(0,3):
+        json = {}
+        json["Model"] = 'RESNET50'
+        json["no"] = i+1
+        json["prediction"] = decoded_prediction_resnet50[0][i][1],
+        json["probability"] = "{:.2%}".format(decoded_prediction_resnet50[0][i][2])
+        model_resnet50.append(json)
+    json_answer.append(model_resnet50)
+
+    return jsonify(json_answer)
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run()
---
+++ static/predictions.js
@@ -0,0 +1,136 @@
+// Hide the button to add a new image until the first result is rendered
+document.getElementsByClassName('show')[1].style.visibility = 'hidden';
+
+// Control the progress in the bar in the html code
+$(function () {
+    var current_progress = 0;
+    var interval = setInterval(function () {
+        current_progress += 10;
+        $("#dynamic")
+            .css("width", current_progress + "%")
+            .attr("aria-valuenow", current_progress)
+            .text(current_progress + "% Complete");
+        if (current_progress >= 90)
+            clearInterval(interval);
+    }, 1000);
+});
+
+// Flask Route that contains the predicted data from the models
+var url = `/predict`;
+
+// Variable initialization for the information from flask for the predictions
+// and probabilities
+var model = [],
+    no = [],
+    prediction = [],
+    probability = [];
+
+// Create arrays for all the data that comes from flask in JSON format
+d3.json(url).then((data) => {
+    // console.log(data)
+    for (i = 0; i < 3; i++) {
+        data[i].forEach((item) => {
+            model.push(item.Model)
+            no.push(item.no)
+            prediction.push(item.prediction)
+            probability.push(item.probability)
+        })
+    }
+
+    // Check the array of information in console
+    // console.log(model),
+    //     console.log(no),
+    //     console.log(prediction),
+    //     console.log(probability);
+
+    // Google library for Gauges 
+    google.charts.load('current', {
+        callback: function () {
+            var container = document.getElementById('chart_div');
+            var chart = new google.visualization.Gauge(container);
+
+            // Convert the probabilities to a float number
+            var p1 = parseFloat(probability[0])
+            var p2 = parseFloat(probability[3])
+            var p3 = parseFloat(probability[6])
+
+            // Verify values of probabilities in console
+            // console.log(p1)
+            // console.log(p2)
+            // console.log(p3)
+
+            // Input the information to the Gauge
+            var data = google.visualization.arrayToDataTable([
+                ['Label', 'Value'],
+                ['VGG16', p1],
+                ['VGG19', p2],
+                ['RESNET50', p3]
+            ]);
+
+            // Create percentage format for the probabilities
+            var formatter = new google.visualization.NumberFormat(
+                { suffix: '%', pattern: '#' }
+            );
+            formatter.format(data, 1);
+            
+            // Options for the Google Gauge Library
+            var options = {
+                min: 0, max: 100,
+                width: 500, height: 255,
+                redFrom: 0, redTo: 25,
+                yellowFrom: 25, yellowTo: 75,
+                greenFrom: 75, greenTo: 100,
+                minorTicks: 10
+            };
+
+            // Render the Gauge
+            chart.draw(data, options);
+        },
+        packages: ['gauge']
+    });
+
+    // Load the Google Library for Charts
+    google.charts.load('current', { 'packages': ['table'] });
+    google.charts.setOnLoadCallback(drawTable);
+
+
+    function drawTable() {
+        // Set the columns of the chart
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'Probability');
+        data.addColumn('string', 'Prediction')
+        data.addColumn('string', 'Model');
+        data.addColumn('number', 'Model Prediction');
+        ;
+
+        // Add the columns to the table from the Flask information in JSON format
+        for (i = 0; i < 9; i++) {
+            data.addRows([
+                [{ v: parseFloat(probability[i]), f: probability[i] }, prediction[i][0], model[i], no[i]]
+            ])
+        }
+
+
+        // sort by column 0, descending
+        data.sort({ column: 0, desc: true });
+
+        // Create the table
+        var table = new google.visualization.Table(document.getElementById('table_div'));
+        table.draw(data, { showRowNumber: true, width: '100%', height: '100%' });
+
+        // Hide the progress bar once the Gauge and Table are created
+        document.getElementsByClassName('hide')[0].style.visibility = 'hidden';
+        // Show the title for the Gauge and Table once the Gauge and Table are created
+        document.getElementsByClassName('show')[0].style.visibility = 'visible';
+        // Show the button for a new image once the Gauge and Table are created
+        document.getElementsByClassName('show')[1].style.visibility = 'visible';
+        // Create the title for the Gauge and Table once the Gauge and Table are created
+        document.getElementsByClassName('show')[0].innerHTML = 'Predictions';
+
+        // Render the information down the Google Gauges that cannot be rendered from the library
+        document.getElementsByClassName('label')[0].innerHTML = prediction[0][0];
+        document.getElementsByClassName('label')[1].innerHTML = prediction[3][0];
+        document.getElementsByClassName('label')[2].innerHTML = prediction[6][0];
+    }
+})
+
